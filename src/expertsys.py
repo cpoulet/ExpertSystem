8626ad9220d5420bf71b501388bf12afe9a6ce73
Cedric Poulet <raccoon@Cedrics-MacBook-Pro.local>
2017-06-16 09:15:02 +0200

    Starting the project

---
+++ src/expertsys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main(argv):
+	if (len(argv) != 2):
+		print ("Wrong number of arguments.")
+		return
+	print("A => B")
+
+if __name__ == "__main__":
+   main(sys.argv)
